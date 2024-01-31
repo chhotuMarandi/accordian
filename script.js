f8483623d91089e36eecc8e8b8d06d1a9d48e8c4
chhotu marandi <cmarandi29@gmail.com>
2024-01-31 18:21:30 +0530

    accordian style

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ const data = [
         question: "Where will your data come from?",
         ans: "Our next step is to identify data sources you need to dig into all your data, pick the fields that you’ll need, leave some space for data you might potentially need in the future, and gather all the information in one place. Be open-minded about your data sources in this step – all departments in your company, sales, finance, IT, etc., have the potential to provide insights."
     },
+    {
+        id: 4,
+        question: "Which scales apply to your different datasets?",
+        ans: "You’ll want to be mindful of the level of measurement for your different variables, as this will affect the statistical techniques you will be able to apply in your analysis."
+    },
+    {
+        id: 4,
+        question: "How can you ensure data quality?",
+        ans: "We already mentioned making sure data is cleaned and prepared to be analyzed is a critical part of it, but there is more. If you want to be successful on this matter, it is necessary to implement a carefully planned data quality management system that involves every relevant data user in the organization as well as data-related processes from acquisition to distribution and analysis"
+    },
 ]
 
 //get Accordian Data
@@ -43,6 +53,7 @@ const accordianTitle = document.querySelectorAll(".title");
 
 accordianTitle.forEach(currentTitle => {
     currentTitle.addEventListener("click",(e)=>{
+        //check if active class is contain or not if not then add them or remove
         if(currentTitle.classList.contains("active")){
             currentTitle.classList.remove("active");
         } else {
@@ -53,6 +64,10 @@ accordianTitle.forEach(currentTitle => {
             });
             
             currentTitle.classList.add("active");
+            let icon = document.querySelector("i");
+            icon.style.transform = "rotate(180deg)";
+            icon.style.transition = "0.3s ease";
+
         }
     })
 });
