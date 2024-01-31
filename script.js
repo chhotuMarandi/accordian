5656e16f944b0509f4701c44dd1b28b903f9339b
chhotu marandi <cmarandi29@gmail.com>
2024-01-31 12:15:59 +0530

    html layout

---
+++ script.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        question: "What exactly do you want to find out?",
+        ans: "It’s good to evaluate the well-being of your business first. Agree company-wide on what KPIs are most relevant for your business and how they already develop. Research different KPI examples and compare them to your own. Think about what way you want them to develop further. Can you influence this development?"
+    },
+    {
+        question: "What standard KPIs will you use that can help?",
+        ans: "OK, let’s move on from the whole genie thing. Sorry, Data Dan! It’s crucial to know what data analysis questions you want to ask from the get-go. They form the bedrock for the rest of this process."
+    },
+    {
+        question: "Where will your data come from?",
+        ans: "Our next step is to identify data sources you need to dig into all your data, pick the fields that you’ll need, leave some space for data you might potentially need in the future, and gather all the information in one place. Be open-minded about your data sources in this step – all departments in your company, sales, finance, IT, etc., have the potential to provide insights."
+    },
+]
